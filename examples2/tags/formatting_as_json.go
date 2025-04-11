ff4518918135fadf0ab75877eac17aaf690cfa17
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-11 11:55:13 +0200

    Formatting data as json

---
+++ examples2/tags/formatting_as_json.go
@@ -0,0 +1,25 @@
+package tags
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Animal struct {
+	Name           string  `json:"animal_name"`
+	ScientificName string  `json:"scientific_name"`
+	Weight         float32 `json:"animal_average_weight"`
+}
+
+func FormattingDataAsJson() {
+	a := Animal{
+		Name:           "cat",
+		ScientificName: "Felis catus",
+		Weight:         10.5,
+	}
+	output, err := json.Marshal(a)
+	if err != nil {
+		panic("couldn't encode json")
+	}
+	fmt.Println(string(output))
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go_structs/examples"
 	"go_structs/examples2"
+	"go_structs/examples2/tags"
 )
 
 func main() {
@@ -14,4 +15,6 @@ func main() {
 	examples.PointerReceiverMethod()
 	fmt.Println()
 	examples2.AnonymousIdentifiers()
+	fmt.Println()
+	tags.FormattingDataAsJson()
 }
